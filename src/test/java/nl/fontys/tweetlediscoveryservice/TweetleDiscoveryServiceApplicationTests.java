9dd1ee079fb2e95ae4bdb6b79f8e1cd01ba3d0ab
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-01 21:12:37 +0200

    Add test profile

---
+++ src/test/java/nl/fontys/tweetlediscoveryservice/TweetleDiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package nl.fontys.tweetlediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TweetleDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
